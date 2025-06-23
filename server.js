d4b65f72e2357d1ba440ab7016fa7559fabf0d99
lhajbouguerra <tayzerll07@gmail.com>
2025-06-23 10:21:44 +0100

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const experss = require('express');
+const cors = require('cors')
 const app = experss()
+app.use(cors())
 app.get("/test", (req, res) => {
     res.json({
-        message:"jwk bh"
+        message: "jwk bh",
+        status: 200
     })
 })
 
